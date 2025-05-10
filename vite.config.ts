7e97770495c1fe2fe79344695cdc407eaedf1f75
unachang113 <hita.d.s2.c@gmail.com>
2025-05-10 23:00:01 +0900

    feat: tailwindcssの追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
